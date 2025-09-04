4dc90d76ced34f5b82753d3c5ab3ca72ad36b67a
xxStevensxx <29457062+xxStevensxx@users.noreply.github.com>
2025-09-04 14:52:18 +0200

    [PROG] [Interactions] : CheckAnswer Behaviour

---
+++ Assets/Scripts/Interactions/CheckAnswers.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+namespace Assets.Scripts.Interactions
+{
+    public class CheckAnswers
+    {
+        [SerializeField, Min(1)] private int nbAnswersRequired;
+        private List<bool> answers = new List<bool>();
+
+
+        public void CheckAnswer(bool answer) {
+
+            answers.Add(answer);
+
+            if (answers.Count >= nbAnswersRequired)
+            {
+                if (answers.All(userAnswer => userAnswer == true))
+                {
+                    GameLoopManager.Instance.LoadNextScene();
+                }
+                else 
+                { 
+                    GameLoopManager.Instance.LoadMenuScene();   
+                }
+            }
+        }
+    }
+}
